1ef78014c39d401c47b53fadcedbf5f7b716a93a
chenxiangqian <641620192@qq.com>
2024-04-03 22:35:24 +0800

    init' git push

---
+++ web/src/layouts/default.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar flat>
+      <v-container class="mx-auto d-flex align-center justify-center">
+        <v-row>
+          <v-col cols="4">
+            <span>🍌 ⋆ 🍒  🎀  小蓝书  🎀  🍒 ⋆ 🍌</span>
+          </v-col>
+          <v-col cols="8">
+           <v-responsive max-width="400">
+             <v-text-field
+               density="compact"
+               label="搜索"
+               rounded="lg"
+               variant="solo-filled"
+               flat
+               hide-details
+               single-line
+               append-inner-icon="mdi-magnify"  
+               @click:appendInner="handleAppendIconClick"
+             ></v-text-field>
+           </v-responsive>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                  <v-list-item
+                    color="grey-lighten-4"
+                    title="发现"
+                    prepend-icon="mdi-text-search"
+                    link
+                    href="/search"
+                  ></v-list-item>
+                  <v-list-item
+                    color="grey-lighten-4"
+                    title="发布"
+                    prepend-icon="mdi-publish"
+                    link
+                  ></v-list-item>
+                  <v-list-item
+                    color="grey-lighten-4"
+                    title="通知"
+                    prepend-icon="mdi-message-alert-outline"
+                    link
+                  ></v-list-item>
+
+
+                <v-divider class="my-2"></v-divider>
+
+                  <v-list-item
+                    color="grey-lighten-4"
+                    title="我"
+                    prepend-icon="mdi-account-circle"
+                    link
+                  ></v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <router-view></router-view>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <AppFooter></AppFooter>
+  </v-app>
+</template>
+
+<script setup>
+import AppFooter from '../components/AppFooter.vue';
+
+  const links = [
+    'Dashboard',
+    'Messages',
+    'Profile',
+    'Updates',
+  ]
+  const handleAppendIconClick=()=>{
+    console.log("click")
+  }
+</script>
---
+++ web/src/pages/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <!-- <HelloWorld /> -->
+  <v-container class="fill-height">
+    <v-row>
+      <v-col>
+         <v-card class="mx-1">  
+         <!-- 使用 v-img 显示图片 -->  
+         <v-img  
+           aspect-ratio="1"  
+           class="pa-4"  
+           src="@/assets/logo.png"  
+           alt="Card Image"  
+         ></v-img>  
+  
+         <!-- 使用 v-card-title 显示标题 -->  
+         <v-card-title class="text-h5">  
+           卡片标题  
+         </v-card-title>  
+  
+         <!-- 使用 v-card-text 显示正文文字 -->  
+         <v-card-text>  
+          答应我，有空气炸锅的都去试试
+         </v-card-text>  
+         </v-card>
+
+      </v-col>
+
+      <v-col>
+         <v-card class="mx-1">  
+         <!-- 使用 v-img 显示图片 -->  
+         <v-img  
+           aspect-ratio="1"  
+           class="pa-4"  
+           src="@/assets/logo.png"  
+           alt="Card Image"  
+         ></v-img>  
+  
+         <!-- 使用 v-card-title 显示标题 -->  
+         <v-card-title class="text-h5">  
+           卡片标题  
+         </v-card-title>  
+  
+         <!-- 使用 v-card-text 显示正文文字 -->  
+         <v-card-text>  
+          答应我，有空气炸锅的都去试试
+         </v-card-text>  
+         </v-card>
+
+      </v-col>
+      <v-col>
+         <v-card class="mx-1">  
+         <!-- 使用 v-img 显示图片 -->  
+         <v-img  
+           aspect-ratio="1"  
+           class="pa-4"  
+           src="@/assets/logo.png"  
+           alt="Card Image"  
+         ></v-img>  
+  
+         <!-- 使用 v-card-title 显示标题 -->  
+         <v-card-title class="text-h5">  
+           卡片标题  
+         </v-card-title>  
+  
+         <!-- 使用 v-card-text 显示正文文字 -->  
+         <v-card-text>  
+          答应我，有空气炸锅的都去试试
+         </v-card-text>  
+         </v-card>
+
+      </v-col>
+      <v-col>
+         <v-card class="mx-1">  
+         <!-- 使用 v-img 显示图片 -->  
+         <v-img  
+           aspect-ratio="1"  
+           class="pa-4"  
+           src="@/assets/logo.png"  
+           alt="Card Image"  
+         ></v-img>  
+  
+         <!-- 使用 v-card-title 显示标题 -->  
+         <v-card-title class="text-h5">  
+           卡片标题  
+         </v-card-title>  
+  
+         <!-- 使用 v-card-text 显示正文文字 -->  
+         <v-card-text>  
+          答应我，有空气炸锅的都去试试
+         </v-card-text>  
+         </v-card>
+
+      </v-col>
+      <v-col>
+         <v-card class="mx-1">  
+         <!-- 使用 v-img 显示图片 -->  
+         <v-img  
+           aspect-ratio="1"  
+           class="pa-4"  
+           src="@/assets/logo.png"  
+           alt="Card Image"  
+         ></v-img>  
+  
+         <!-- 使用 v-card-title 显示标题 -->  
+         <v-card-title class="text-h5">  
+           卡片标题  
+         </v-card-title>  
+  
+         <!-- 使用 v-card-text 显示正文文字 -->  
+         <v-card-text>  
+          今天吃的饭
+         </v-card-text>  
+         </v-card>
+
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
